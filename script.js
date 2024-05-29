eed265afb9158ebd90012d2b3ef8645c17ffc257
Imre <rabl.imre95@gmail.com>
2024-05-29 18:07:51 +0200

    added hello world functions

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello world")
+console.log("hello world")
+
+function helloWorld() {
+    console.log("hello world from the function")
+}
+
+console.log("hello world 2")
+
+helloWorld();
+
+const coolerHelloWorld = function() {
+    console.log("hello world from the cooler function");
+}
+
+coolerHelloWorld();
+
