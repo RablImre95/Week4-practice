3ca2a6f5781c87891e0e183dea81c14f6def0401
Imre <rabl.imre95@gmail.com>
2024-06-03 20:03:21 +0200

    did cb methods

--- script.js
+++ script.js
@@ -1,48 +1,52 @@
-/* console.log("hello world")
+const data = ["one", "two", "three", "four", "five", "six", "seven"]
 
-function helloWorld() {
-    console.log("hello world from the function")
-}
+/*data.forEach((element, index, array) => {
+    console.log(`The ${index}. element of the "${array}" array is: ${element} `)    
+}) */
 
-console.log("hello world 2")
+//FOREACH - DOES NOT HAVE RETURN VALUE --> UNDEFINED
 
-helloWorld();
+/*const newData = [];
+const result = data.forEach(element => newData.push(`new${element}`));
+console.log(newData) */
 
-const coolerHelloWorld = function() {
-    console.log("hello world from the cooler function");
-}
+/*const result = data.map((element, index, array) => {
+    console.log(`The ${index}. element of the "${array}" array is: ${element} `)
+})
+console.log(result); */
 
-coolerHelloWorld();
+// MAP - ALWAYS HAS RETURN VALUE --> ARRAY WITH SAME LENGTH AS ORIGINAL ARRAY
+/*data.map(element => `new${element}`)
+console.log(result); */
 
-const coolestHelloWorld = () => {
-    console.log("hello world from the coolest function")
-}
+/*const result = data.filter((element) => element.length > 3)
+console.log(result) */
 
-coolestHelloWorld(); */
+// const result = data.reduce((prev,curr) => prev + curr, "jéghidegmitiszol");
+/*
+1. prev: "jéghidegmitiszol"
+    curr: "one"
+    callback: pre + curr -> "jéghidegmitiszol + "one" -> "jéghidegmitiszolone"
+2. prev: "jéghidegmitiszol"
+    curr: "one"
+    callback: pre + curr -> "jéghidegmitiszol + "two" -> "jéghidegmitiszolwo"
 
-function sumOfTwoNumbers(number1, number2) {
-    return number1 + number2;
-}
+*/
+// console.log(result)
 
-function multiplyTwoNumbers(number1, number2) {
-    return number1 * number2
-}
+// const result = data.reduce((prev,curr) => prev + curr);
+/*
+1. prev: "one"
+    curr: "two"
+    callback: prev + current -> "one" + "two" -> "onetwo"
+2. prev: "onetwo"
+    curr: "tthree"
+    callback: prev + current -> "onetwo" + "three" -> "onetwothree"
+*/
+// console.log(result)
 
-/*const result = sumOfTwoNumbers(5, 10);
-console.log("result: ", result); */
+/*const result = data.find(element => element.length > 3);
+console.log(result); */
 
-/*console.log(sumOfTwoNumbers(20, 2));
-
-const arrowSumOfTwoNumbers= (number1, number2) => number1 + number2;
-
-console.log(arrowSumOfTwoNumbers(1, 2)); */
-
-function doSomeMagic(number1, number2, callback) {
-    return callback(number1, number2)
-}
-console.log(doSomeMagic(1, 2, sumOfTwoNumbers));
-console.log(doSomeMagic(1, 2, multiplyTwoNumbers));
-console.log(doSomeMagic(10, 2, (number1, number2) => number1**number2))
-console.log(doSomeMagic(10, 2, function (number1, number2) {
-    return number1**number2
-}));
+const result = data.findIndex(element => element.length > 3);
+console.log(result);
